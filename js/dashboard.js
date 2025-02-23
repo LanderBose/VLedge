bd12dcee022907c31149f3a350dd98faa132a9be
Lloyd <mangilog.lloyd.bigot@gmail.com>
2025-02-24 01:21:10 +0800

    inital commit

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+function updateDateTime() {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+    const formattedTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    document.querySelector('.date-time').innerHTML = `${formattedDate} &nbsp; ${formattedTime}`;
+}
+setInterval(updateDateTime, 1000);
+window.onload = updateDateTime;
